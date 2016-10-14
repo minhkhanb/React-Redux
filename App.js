65c833a19fb23e9416f7fcea24ac738b134bc800
KhaLe <kha.le@mtech.vn>
2016-10-14 09:29:31 +0700

    update config

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from 'components/Header';
-import Content from 'components/Content';
+import Header from './components/Header';
+import Content from './components/Content';
 
 class App extends React.Component {
     render() {
@@ -8,11 +8,9 @@ class App extends React.Component {
             <div>
                 <Header/>
                 <Content/>
-                <p data-myattribute="Attribute">This is the content 333!sdfsdfds!!</p>
             </div>
         );
     }
 }
 
 export default App;
-
