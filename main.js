7b67d40f51e4c19983226c8a19383a6d3dcf13d2
KhaLe <kha.le@mtech.vn>
2016-10-17 15:34:20 +0700

    redux

--- main.js
+++ main.js
@@ -5,17 +5,26 @@ import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import totoApp from './reducers/reducers';
+
+let store = createStore(totoApp);
+
 //ReactDOM.render(<App />, document.getElementById('app'));
 
 ReactDOM.render(
     (
-        <Router history={browserHistory}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+        /*<Router history={browserHistory}>
             <Route path="/" component={App}>
-                <IndexRoute component={Home}/>
-                <Route path="home" component={Home}/>
-                <Route path="about" component={About}/>
-                <Route path="contact" component={Contact}/>
+            <IndexRoute component={Home}/>
+            <Route path="home" component={Home}/>
+            <Route path="about" component={About}/>
+            <Route path="contact" component={Contact}/>
             </Route>
-        </Router>
+        </Router>*/
     ), document.getElementById('app')
 );
